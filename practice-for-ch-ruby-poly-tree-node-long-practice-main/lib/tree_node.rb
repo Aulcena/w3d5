c8b1ebf85162940519803276d28b8316bd6d4009
AJU <aulcena@gmail.com>
2023-07-14 15:11:23 -0400

    debugging parents method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,6 +1,8 @@
+
+require "byebug"
 class PolyTreeNode
 
-    attr_reader :parent, :children, :value
+    attr_accessor :parent, :children, :value
 
     def initialize(value, children = [])
         @parent = nil
@@ -14,8 +16,32 @@ class PolyTreeNode
     end 
 end
 
+
+
+
+# account for old parent is not nil and new parent is nil 
+
 def parent=(parent) 
+    
+    # delete myself parents list of children
+    # account for old parent is nil and new parent is not nil
+    self.parent.children.delete(self) if self.parent != nil 
+    
+    debugger
     @parent = parent
-    @parent.children << self # same as self.parent .... or parent
+
+    # assign a parent and assign our(new) parents children
+
+    if self.parent != nil
+        if !@parent.children.include?(self)
+            
+            @parent.children << self # same as self.parent .... or parent
+
+            p @parent.children
+        end
+    end
+
 
 end
+p s = PolyTreeNode.new(3)
+
