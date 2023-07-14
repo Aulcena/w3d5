6cfe70217ebfefc85b2fff7a9fcb4576371dc567
Dan Lee <danielhlee4@gmail.com>
2023-07-14 14:11:42 -0400

    add #initialize

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
-  
+
+    attr_reader :parent, :children, :value
+
+    def initialize(value, children = [])
+        @parent = nil
+        @value = value
+        @children = children
+    end
+
+    def parent=(parent) 
+        @parent = parent
+
+    end 
+end
+
+def parent=(parent) 
+    @parent = parent
+    @parent.children << self # same as self.parent .... or parent
+
 end
